2c7da61c5241860c98770cdbb50b803af4bf100e
Vladimir Aseev <el-radiance@bk.ru>
2018-11-01 16:20:12 +0100

    Интерфейс анализатора стоимости модифицирован по согласованию

--- src/core/ElevatorExpensesAnalyser.java
+++ src/core/ElevatorExpensesAnalyser.java
@@ -16,23 +16,13 @@ public interface ElevatorExpensesAnalyser {
      * Учитывает необходимое время на открывание дверей в точке назначения
      *
      * @param elevator лифт, для которого будет произведён рассчёт
-     * @param desiredDestinationFloorNumber целевой этаж, количество тиков до которого считается
+     * @param takePassengersFromFloor этаж, с которого нужно забрать пассажиров
+     * @param deliverPassengersToFloor этаж, на который нужно доставить пассажиров
      * @return количество тиков
      */
-    int calculateExpenses(Elevator elevator, int desiredDestinationFloorNumber);
+    int calculateExpenses(Elevator elevator,
+                          int takePassengersFromFloor,
+                          int deliverPassengersToFloor,
+                          Passenger... passengers);
 
-    /**
-     * Рассчитывает стоимость перемещения лифта до заданного этажа {@code desiredDestinationFloorNumber}
-     * единицах времени (в тиках)
-     * Учитывает время, необходимое для завершения текущего действия
-     * Учитывает текущее состояние лифта (открыты/закрыты ли двери и т.п.)
-     * Учитывает необходимое время на открывание дверей в точке назначения
-     *
-     * @param elevator лифт, для которого будет произведён рассчёт
-     * @param desiredDestinationFloorNumber целевой этаж, количество тиков до которого считается
-     * @param passengers пассажиры, время на посадку и перемещение которых следует учесть дополнительно
-     *                   к пассажирам уже находящемся в лифте
-     * @return количество тиков
-     */
-    int calculateExpenses(Elevator elevator, int desiredDestinationFloorNumber, Passenger... passengers);
 }
