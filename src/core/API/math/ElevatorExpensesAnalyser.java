c84b6ecbe318849eefb7b1a338ef30676b432a87
Vladimir Aseev <el-radiance@bk.ru>
2018-11-01 13:38:30 +0100

    Расширен интерфейс стоимостного анализатора

--- src/core/API/math/ElevatorExpensesAnalyser.java
+++ src/core/API/math/ElevatorExpensesAnalyser.java
@@ -1,6 +1,7 @@
 package core.API.math;
 
 import core.API.Elevator;
+import core.API.Passenger;
 
 /**
  * Рассчитывает стоимость действий лифта
@@ -18,4 +19,17 @@ public interface ElevatorExpensesAnalyser {
      * @return количество тиков
      */
     int calculateExpenses(Elevator elevator, int desiredDestinationFloorNumber);
+
+    /**
+     * Рассчитывает стоимость перемещения лифта до заданного этажа {@code desiredDestinationFloorNumber}
+     * единицах времени (в тиках)
+     * Учитывает текущее состояние лифта (открыты/закрыты ли двери и т.п.)
+     * Учитывает необходимое время на открывание дверей в точке назначения
+     *
+     * @param elevator лифт, для которого будет произведён рассчёт
+     * @param desiredDestinationFloorNumber целевой этаж, количество тиков до которого считается
+     * @param passengers пассажиры, время на посадку которых следует учесть
+     * @return количество тиков
+     */
+    int calculateExpenses(Elevator elevator, int desiredDestinationFloorNumber, Passenger... passengers);
 }
